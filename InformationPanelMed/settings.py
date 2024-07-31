419871cb8d9620857be101a247a8cf2bb8164cf6
Dmitriy Rodionov <dmitr.rod@gmail.com>
2024-07-31 19:47:22 +0300

    добавлен sqlite

--- InformationPanelMed/settings.py
+++ InformationPanelMed/settings.py
@@ -69,15 +69,20 @@ WSGI_APPLICATION = 'InformationPanelMed.wsgi.application'
 env('SECRET_KEY')
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': env('DB_NAME'),
+#         'USER': env('DB_USER'),
+#         'PASSWORD': env('DB_PASSWORD'),
+#         'HOST': env('DB_HOST'),
+#         'PORT': env('DB_PORT'),
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
